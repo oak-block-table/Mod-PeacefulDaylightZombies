2c10746df58ff0ce8ec557e86a700bb08def7ba0
Oak Table <none>
2022-07-16 09:59:32 -0700

    Overwrite the burnsInDaylight() method - v0.0.3

--- src/main/java/org/hexagonalmagmacube/daylightzombie/mixin/MixinZombieEntity.java
+++ src/main/java/org/hexagonalmagmacube/daylightzombie/mixin/MixinZombieEntity.java
@@ -3,7 +3,6 @@ package org.hexagonalmagmacube.daylightzombie.mixin;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.ActiveTargetGoal;
-import net.minecraft.entity.ai.goal.PrioritizedGoal;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.passive.MerchantEntity;
 import net.minecraft.entity.passive.TurtleEntity;
@@ -14,7 +13,7 @@ import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.ZombieEntity;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -28,16 +27,19 @@ public abstract class MixinZombieEntity extends HostileEntity {
     private static double GroundLevelThreshold = 60.0;
     // Hostile Entity will become peaceful in a brightness above this level (Range = 0.0 to 1.0 ?)
     // When measured within a ring of torches, the brightness level was 0.41.
-    // We will assume a torch-lit room is at least 0.3f
-    private static float CalmingBrightnessThreshold = 0.3f;
+    // Tested a few reasonably lit rooms and found brightness as low as 0.27 in corners.
+    // We will assume a torch-lit room is at least 0.25f
+    private static float CalmingBrightnessThreshold = 0.25f;
 
 
     protected MixinZombieEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
     }
 
-    @Shadow
-    public boolean canBreakDoors() { return false; }
+    @Overwrite
+    protected boolean burnsInDaylight() {
+        return false;
+    }
 
     @Inject(method = "initCustomGoals()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ai/goal/GoalSelector;add(ILnet/minecraft/entity/ai/goal/Goal;)V",
             ordinal = 4), cancellable = true)
@@ -62,9 +64,6 @@ public abstract class MixinZombieEntity extends HostileEntity {
             super(zombie, targetEntityClass, true);
         }
 
-        // Assume an entity is above ground if it is above this block level
-        private static int NumberTicksBetweenChecks = 40;
-
         private int i = 0;
         public boolean canStart() {
             // First evaluate
@@ -73,21 +72,14 @@ public abstract class MixinZombieEntity extends HostileEntity {
                 return false;
             }
 
-            // Considered evaluating these conditions every N clock ticks to avoid server lag.
-            // However, super.canStart() already contains a randomizer to skip some
-//            i++;
-//            if (i%NumberTicksBetweenChecks != 0) {
-//                return false;
-//            }
-//            i = 0;
-
             // TODO: Should we try to use this.mob.getEyeLocation().getBlock().getType() != Cave Air instead
             boolean isAboveGround = (this.mob.getY() > GroundLevelThreshold);
             if (!isAboveGround) { return true; }
             boolean isBrightEnough = (this.mob.getBrightnessAtEyes() > CalmingBrightnessThreshold);
 
-//            PeacefulDaylightZombiesMod.LOGGER.info(String.format("Y-level: %.2f; Brightness: %.2f; bright Enough: %b",
-//                    this.mob.getY(), this.mob.getBrightnessAtEyes(), isBrightEnough));
+//            PeacefulDaylightZombiesMod.LOGGER.info(String.format("Y-level: %.2f; Brightness: %.2f; bright Enough: %b; (%s)",
+//                    this.mob.getY(), this.mob.getBrightnessAtEyes(), isBrightEnough,
+//                    this.mob.getUuidAsString().substring(0,7)));
 
             if (isBrightEnough) {
                 this.targetEntity = null;
@@ -95,7 +87,11 @@ public abstract class MixinZombieEntity extends HostileEntity {
             }
             if (this.targetEntity != null) {  // null check, just in case
                 PeacefulDaylightZombiesMod.LOGGER.info(String.format(
-                        "Zombie has targeted a %s", this.targetEntity.getClass().getSimpleName()));
+                        "Zombie %s targets a %s; [X,Y,Z]: [%.1f, %.1f, %.1f]; Brightness: %.3f",
+                        this.mob.getUuidAsString().substring(0,7),
+                        this.targetEntity.getClass().getTypeName(),
+                        this.mob.getX(), this.mob.getY(), this.mob.getZ(),
+                        this.mob.getBrightnessAtEyes()));
             }
             return true;
         }
